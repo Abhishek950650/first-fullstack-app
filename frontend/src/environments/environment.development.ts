0764d6f849c63e2874c43bdfd307438e08d89d3e
Abhishek Kumar <abhishek950650@gmail.com>
2026-06-13 15:54:56 +0530

    updated

--- frontend/src/environments/environment.development.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-    production: false,
-    apiUrl : 'http://127.0.0.1:5000/api/auth',  // Flask backend URL
-    aesKey: 'abhishek-aes-key-exactly-32chars'  // same as backend
-};
